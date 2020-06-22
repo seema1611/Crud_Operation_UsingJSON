2ce2767391bbdaabab6fa3c05799a296c8145959
seema1611 <seemarajpure16@gmail.com>
2020-06-23 05:10:31 +0530

    ADDED: Feature to create model class

---
+++ src/main/java/com/jsondemo/JsondemoApplication.java
@@ -0,0 +1,13 @@
+package com.jsondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsondemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsondemo/JsondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsondemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
